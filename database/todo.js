873ca98aed41116f28ac5ca9139c9032d82ac7bc
S-Teuling <98848070+S-Teuling@users.noreply.github.com>
2022-11-10 11:38:37 +0100

    Add files via upload

---
+++ database/todo.js
@@ -0,0 +1,5 @@
+export const _ = [  ];
+_[0] = { 'description': `Finish Front-End Development Course`, 'done': false, '_id': `b6f9fc14-4d3c-4dc7-be40-12423fb503b8`, '_createdOn': `2022-11-09T13:05:21.195Z` };
+_[1] = { 'description': `Was Car`, 'done': false, '_id': `4ae0f270-ca2b-4f96-b6d0-0c9ac163cbf0`, '_createdOn': `2022-11-09T13:05:30.481Z` };
+delete _[1];
+_['length'] = 1;
